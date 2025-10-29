931f8d751da8d68de06b336fd4e2c988308b9f8f
Dmytro Shulha <dimashulga07@gmail.com>
2025-10-29 23:59:42 +0200

    _

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
@@ -11,8 +12,12 @@ public class HelloApplication extends Application {
         FXMLLoader mainLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene mainScene = new Scene(mainLoader.load(), 600, 550);
         stage.setTitle("Hello!");
+        mainScene.getStylesheets().add(getClass().getResource("my.css").toExternalForm());
         stage.setScene(mainScene);
         stage.show();
+
+
+
     }
 
     public static void main(String[] args) {
