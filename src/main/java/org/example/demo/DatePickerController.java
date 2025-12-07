8aeb3afe39f1006bee4f417f8465456dd38ff3f5
Dmytro Shulha <dimashulga07@gmail.com>
2025-12-07 18:10:23 +0200

    _

--- src/main/java/org/example/demo/DatePickerController.java
+++ src/main/java/org/example/demo/DatePickerController.java
@@ -1,12 +1,23 @@
 package org.example.demo;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
 
-import java.awt.event.ActionEvent;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 
 public class DatePickerController {
 
@@ -15,6 +26,21 @@ public class DatePickerController {
 
     @FXML
     private DatePicker myDatePicker;
+
+    @FXML
+    private ColorPicker myColorPicker;
+
+    @FXML
+    private AnchorPane scenePane;
+
+    @FXML
+    private Slider mySlider;
+
+    @FXML
+    private Label sliderLabel;
+
+    private int score;
+
 
 
     @FXML
@@ -30,7 +56,27 @@ public class DatePickerController {
         });
 
 
+        mySlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+
+                score = (int) mySlider.getValue();
+                sliderLabel.setText(score + " Балів");
+
+            }
+        });
+
+
     }
+
+    @FXML
+    void changeColor(ActionEvent event) {
+        Color myColor = myColorPicker.getValue();
+        scenePane.setBackground(new Background(new BackgroundFill(myColor,null, null)));
+    }
+
+
+
 
 
 
